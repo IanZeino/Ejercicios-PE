19dd3456bfc0bb764a4581bd33d058e8d83057a9
Ian Zea <izeac.smmc@gmail.com>
2020-10-27 13:27:41 -0600

    Añadí mi ejercicio numero 2 de Proyect euler :v

---
+++ Fibonacci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Secuencia_de_fibonacci
+{
+    class SumaFiboPar 
+    {
+        static void Main(string[] args)
+        {
+            int num3 = 0;
+            int num6 = 2;
+            int secuencia = 0;
+            int sumaSecuenciaPar = 0;
+            
+            while(secuencia < 4000000){
+                sumaSecuenciaPar += secuencia;
+                secuencia = 4 * num3 + num6;
+                num6 = num3;
+                num3 = secuencia;
+                Console.WriteLine(sumaSecuenciaPar);
+            }
+        }
+    }
+}

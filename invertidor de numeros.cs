7c651755841edf71a82ab2f1b781e336b230f294
Ian Zea <izeac.smmc@gmail.com>
2020-10-27 22:29:55 -0600

    Añadi mi tercer ejercicio de PE

---
+++ invertidor de numeros.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Invierte_numeros
+{
+    class soremun
+    {
+        static void Main(string[] args)
+        {                
+            int temporal = inverticion(589001);
+            Console.WriteLine(temporal);
+        }
+        public static int inverticion (int num)
+        {
+            int numerillo = num;
+            int nuevoNum = 0;
+            string numTextual = Convert.ToString(numerillo);
+            int contah = numTextual.Length - 1;
+            char[] cifras = new char[numTextual.Length]; 
+
+            foreach(char i in numTextual)
+            {
+                cifras[contah] = i;
+                contah--;
+            }
+            numTextual = new string(cifras);
+            nuevoNum = Convert.ToInt32(numTextual);
+
+            return nuevoNum;
+        }
+    }
+}

62a300182af55c9bf97d5f51fc1c4e1c3b5dc00c
Ian Zea <izeac.smmc@gmail.com>
2021-01-12 19:15:20 -0600

    se agrego una nueva carpeta de ejercicios con un ejercicio en ella XD

---
+++ Ejercicios bizarros/Obten los sumandos de la suma mas alta xd.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace sumate_las_chivas
+{
+    class suma_4
+    {
+        static void Main(string[] args){
+            string numero = "134789997314810";
+            int indice = 0;
+            string trozo;
+            int textonum = 0;
+            int suma = 0;
+            int valorfin = 0;
+            string digitos = "";
+
+            while(indice < 12){
+                trozo= numero.Substring(indice,4);
+                textonum = int.Parse(trozo);
+                indice++;
+                while(textonum > 0){
+                    suma = suma + textonum % 10;
+                    textonum = textonum/10;
+                }
+                if (valorfin < suma){
+                    valorfin = suma;
+                    digitos = trozo;
+                }
+                suma = 0;
+            }
+            Console.WriteLine(digitos);
+        }
+    }
+}
+
+

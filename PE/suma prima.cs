1cc5175560ea617cc100981fd40d1ed7e1cd4aee
Ian Zea <izeac.smmc@gmail.com>
2021-01-16 21:30:05 -0600

    ejercicio 10 PE

---
+++ PE/suma prima.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace sumate_los_primos
+{
+    class suma
+    {
+        static void Main(string[] args){
+            int n = 3;
+            long sumaPrima = 2;
+            bool compruebaPrim = true;
+            int divisores = 2;
+            
+            while(n < 2000_000)
+            {
+                while(divisores*divisores <= n) 
+                {
+                    if(n%divisores == 0)
+                    {
+                        compruebaPrim = false;
+                        break;
+                    }
+                    divisores++;
+                }
+                if(compruebaPrim)
+                {
+                    sumaPrima+=n;
+                }
+                compruebaPrim = true;
+                divisores=2;
+                n++;
+            }
+            Console.WriteLine(sumaPrima);
+        }
+    }
+}
+
+
+
+
+
